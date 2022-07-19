0cb51fb09b6ce6fe41efd361c1f0889369680823
Ioana Gheorghias <ioana.gh27@gmail.com>
2022-07-19 08:58:48 +0100

    Add server

---
+++ client/src/index.js
@@ -0,0 +1,41 @@
+// function displayPage(data) {
+//     const body = document.getElementsByTagName('body');
+
+//     body.textContent = data;
+// }
+
+
+// function loadPage() {
+
+//     const url = "http://localhost:3000";
+
+//     fetch(url)
+//     .then(res => res.json())
+//     .then(data => displayPage(data))
+//     .catch(err => {
+//         console.log(err)
+
+//     })
+
+
+// }
+
+// loadPage();
+
+// const quotes = require("./quotes");
+
+// function generateQuote() {
+
+//     fetch(quotes)
+//     .then(res => res.json())
+//     .then(data => document.getElementById('random-quote').textContent = data)
+//     .catch(err => {
+//         console.log(err)
+//     })
+// }
+
+// setInterval(generateQuote(), 5000);
+
+
+
+
---
+++ client/src/quotes.js
@@ -0,0 +1,16 @@
+const quotes = [
+    {
+        author: "Mary Engelbreit",
+        quote: "Walk towards the sunshine, and the shadows will fall behind you."
+    },
+    {
+        author: "Epitectus",
+        quote: "There is only one way to happiness and that is to cease worrying about things which are beyond the power or our will."
+    },
+    {
+        author: 	"Hans Christian Andersen",
+        quote: 	"Everything you look at can become a fairy tale and you can get a story from everything you touch."
+    }
+]
+
+module.exports = quotes;
